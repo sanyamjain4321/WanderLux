d9fdfbd8d19ac08ac856ceac85c41cbd7bbb7a95
MalviSood <malvisood@gmail.com>
2026-05-28 01:39:01 +0530

    initial commit

--- wanderlux_fixed/backend/routes/packages.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getAllPackages, getPackageById } = require('../controllers/packagesController');
-
-router.get('/', getAllPackages);
-router.get('/:id', getPackageById);
-
-module.exports = router;
--- wanderlux_fixed/backend/routes/transport.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getTransportOptions } = require('../controllers/transportController');
-
-// GET /api/transport?type=domestic|international
-router.get('/', getTransportOptions);
-
-module.exports = router;
